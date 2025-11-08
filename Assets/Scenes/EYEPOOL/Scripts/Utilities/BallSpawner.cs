33d645a68a9ac24d48c2b9f89e7c145e665f4387
Oscar San <oscarsani007@gmail.com>
2025-11-07 22:13:03 -0500

    Energy Ball Spawner Rework
    - 20 balls max per round (amount of 20 only every spawn round)
    - Spawns 5 balls for each colour randomly
    - Stations now only accept 5 balls each
    - Reset Round function added

--- Assets/Scenes/EYEPOOL/Scripts/Utilities/BallSpawner.cs
+++ Assets/Scenes/EYEPOOL/Scripts/Utilities/BallSpawner.cs
@@ -12,11 +12,17 @@ public class BallSpawner : MonoBehaviour
     [SerializeField] private float minimumPresence = 1f;
     private Dictionary<int, Coroutine> presenceTimers = new Dictionary<int, Coroutine>();
 
+    [Header("Contaiment Box Population")]
+    public int redBallCount = 0;
+    public int yellowBallCount = 0;
+    public int greenBallCount = 0;
+    public int purpleBallCount = 0;
 
     [Header("Ball Spawn Settings")]
     [SerializeField] private BallPalette ballPalleteAsset;
     [SerializeField] private int ballsPerPerson = 4;
     [SerializeField] private int maxBallsInRoom = 20;
+    [SerializeField] private int ballsLeftToSpawn;
     [SerializeField] private int ballCount;
     [SerializeField] private bool zeroFlag;
     private static BallPalette.Entry[] ballPalette;
@@ -38,6 +44,7 @@ public class BallSpawner : MonoBehaviour
     {
         ballPalette = ballPalleteAsset.GetEntries();
         sinkBoundary = Mathf.Abs(xRange.x) - 5f;
+        ballsLeftToSpawn = maxBallsInRoom;
         audioManager = FindAnyObjectByType<AudioManager>();
     }
 
@@ -53,7 +60,7 @@ public class BallSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ballCount <= 0 && !zeroFlag)
+        if (ballCount <= 0 && !zeroFlag && ballsLeftToSpawn > 0)
         {
             zeroFlag = true;
             StartCoroutine(NewPlayerBallSpawn());
@@ -62,16 +69,63 @@ public class BallSpawner : MonoBehaviour
         {
             zeroFlag = false;
         }
+        // if( ballsLeftToSpawn <= 0 && ballCount <= 0)
+        // {
+        //     ResetTerminalGoals();
+        // }
     }
 
     private void SpawnEnergyBall()
     {
         int ballSeed = Random.Range(0, ballPalette.Length);
-        Vector3 pos = new(GetRandomXPos(), -0.25f, GetRandomZPos());
+        bool found = false;
+        switch (ballSeed)
+        {
+            case 0:
+                if (purpleBallCount < 5)
+                {
+                    purpleBallCount++;
+                    found = true;
+                }
+                break;
+            case 1:
+                if (greenBallCount < 5)
+                {
+                    greenBallCount++;
+                    found = true;
+                }
+                break;
+            case 2:
+                if (yellowBallCount < 5)
+                {
+                    yellowBallCount++;
+                    found = true;
+                }
+                break;
+            case 3:
+                if (redBallCount < 5)
+                {
+                    redBallCount++;
+                    found = true;
+                }
+                break;
+            default:
+                if(ballsLeftToSpawn > 0 && ballCount < maxBallsInRoom)
+                {
+                    SpawnEnergyBall();   
+                }
+                break;
+        }
 
-        GameObject newEnergyBall = Instantiate(ballPalette[ballSeed].prefab, pos, ballPalette[ballSeed].prefab.transform.rotation);
-        newEnergyBall.AddComponent<EnergyBall>();
-        newEnergyBall.GetComponent<EnergyBall>().Initialise(newEnergyBall, ballSeed, ballPalette[ballSeed].material.color, ballPalette[ballSeed].captureSprite, ballPalette[ballSeed].spawnFX, this);
+        if(found)
+        {
+            ballCount++; // Confirmed new ball spawn
+            ballsLeftToSpawn--;
+            Vector3 pos = new(GetRandomXPos(), -0.25f, GetRandomZPos());
+            GameObject newEnergyBall = Instantiate(ballPalette[ballSeed].prefab, pos, ballPalette[ballSeed].prefab.transform.rotation);
+            newEnergyBall.AddComponent<EnergyBall>();
+            newEnergyBall.GetComponent<EnergyBall>().Initialise(newEnergyBall, ballSeed, ballPalette[ballSeed].material.color, ballPalette[ballSeed].captureSprite, ballPalette[ballSeed].spawnFX, this);   
+        }
 
 
         // Audio Manager Play
@@ -116,22 +170,24 @@ public class BallSpawner : MonoBehaviour
 
     private IEnumerator ConfirmPresenceAfterDelay(AugmentaObject obj, int id)
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
 
-        // If we're still tracking the object after 2.5 seconds, they didn't leave
+        // If we're still tracking the object after 5 seconds, they didn't leave
         if (presenceTimers.ContainsKey(id))
         {
             presenceTimers.Remove(id);
-            StartCoroutine(NewPlayerBallSpawn());
+            if( ballsLeftToSpawn > 0)
+            {
+                StartCoroutine(NewPlayerBallSpawn());                   
+            }
         }
     }
 
     public IEnumerator DelayedBallSpawn(float manDelay)
     {
         yield return new WaitForSeconds(minimumPresence);
-        if (!(ballCount >= maxBallsInRoom))
+        if (ballsLeftToSpawn > 0)
         {
-            ballCount++;
             yield return new WaitForSeconds(Random.Range(1f, 5f)); // time between ball spawns
             SpawnEnergyBall();
         }
@@ -141,7 +197,7 @@ public class BallSpawner : MonoBehaviour
     {
         for (int i = 0; i < ballsPerPerson; i++)
         {
-            if (ballCount < maxBallsInRoom)
+            if (ballsLeftToSpawn > 0)
             {
                 StartCoroutine(DelayedBallSpawn(0f));
             }
@@ -150,6 +206,16 @@ public class BallSpawner : MonoBehaviour
                 yield return null;
             }
         }
+    }
+
+    private void ResetTerminalGoals()
+    {
+        redBallCount = 0;
+        yellowBallCount = 0;
+        greenBallCount = 0;
+        purpleBallCount = 0;
+        ballCount = 0;
+        ballsLeftToSpawn = maxBallsInRoom;
     }
 
     public float GetSinkBoundary()
